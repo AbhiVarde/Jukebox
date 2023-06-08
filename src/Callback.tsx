752b86c8f6c34780d317ea2564097af661e2e03e
AbhiVarde <91776428+AbhiVarde@users.noreply.github.com>
2023-06-08 17:03:27 +0530

    initial commit

--- src/Callback.tsx
+++ src/Callback.tsx
@@ -10,7 +10,7 @@ const Callback = () => {
     const handleCallback = async () => {
       const searchParams = new URLSearchParams(location.search);
       const code = searchParams.get("code");
-      const redirect_uri = "https://jukebox-xi.vercel.app/callback";
+      const redirect_uri = "http://localhost:3000/callback";
       const client_id = "aeb66ebb7dc140d8bda26cc47b2a195d";
       const client_secret = "1339a2cf989e471b8a746f72245f67a3";
 
--- src/Login.tsx
+++ src/Login.tsx
@@ -8,7 +8,7 @@ const Login = () => {
   const handleLogin = () => {
     const client_id = "aeb66ebb7dc140d8bda26cc47b2a195d";
     const scope = "user-read-private user-read-email";
-    const redirect_uri = "https://jukebox-xi.vercel.app/callback";
+    const redirect_uri = "http://localhost:3000/callback";
 
     window.location.href = `https://accounts.spotify.com/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${encodeURIComponent(
       scope
