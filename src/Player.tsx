50f7b68c93d3853ca0440294760290d53a00ea19
AbhiVarde <91776428+AbhiVarde@users.noreply.github.com>
2023-10-28 21:26:10 +0530

    update footer

--- src/Player.tsx
+++ src/Player.tsx
@@ -19,7 +19,6 @@ const Player = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const audioRef = useRef<HTMLAudioElement>(null);
-  const currentYear = new Date().getFullYear();
 
   const handleSearch = async () => {
     if (!searchTerm) return;
@@ -270,8 +269,15 @@ const Player = () => {
         </div>
       )}
 
-      <footer className="mx-4 text-xs sm:text-sm lex justify-center items-center text-center text-gray-900 py-4">
-        © {currentYear} AbhiVarde - Made with ❤️ for the people of the internet.
+      <footer className="mx-4 text-xs sm:text-sm lex justify-center items-center text-center text-gray-600 py-4">
+        &copy; {new Date().getFullYear()}, Designed & Build by{" "}
+        <a
+          href="https://abhivarde.vercel.app"
+          className=" text-black font-semibold"
+        >
+          AbhiVarde
+        </a>
+        .{" "}
       </footer>
     </div>
   );
